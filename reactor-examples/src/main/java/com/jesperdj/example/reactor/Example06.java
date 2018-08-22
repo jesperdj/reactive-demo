068f85ebfea4e2f8049e25df7c361d6a6ddc7fe8
Jesper de Jong <jespdj@gmail.com>
2018-08-22 22:18:14 +0200

    Example06 code formatting

--- reactor-examples/src/main/java/com/jesperdj/example/reactor/Example06.java
+++ reactor-examples/src/main/java/com/jesperdj/example/reactor/Example06.java
@@ -3,6 +3,7 @@ package com.jesperdj.example.reactor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
@@ -16,7 +17,9 @@ public class Example06 {
     public static void main(String[] args) throws InterruptedException {
         Flux<Long> ticks = Flux.interval(Duration.ofMillis(500));
 
-        ticks.publishOn(Schedulers.newSingle("tick", true))
+        Scheduler scheduler = Schedulers.newSingle("tick", true);
+
+        ticks.publishOn(scheduler)
                 .subscribe(n -> LOG.info("Tick {}", n));
 
         Thread.sleep(5000L);
